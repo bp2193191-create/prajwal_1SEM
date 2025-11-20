7d69d27c21f480d9675354a398cf677caeb70cdf
bp2193191-create <bp2193191@gmail.com>
2025-11-20 15:52:27 +0530

    day4 code pushed

---
+++ day 4/2_prog.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+    int age=30;
+    printf("your age is:%d",age);
+    float marks=345.34;
+    printf("\nyour marks is:%f",marks);
+    double pi=3.14;
+    printf("\n value of pi is:1f",pi);
+    char gender="m";
+    printf("your gender is:%c",gender);
+    bool iswalking = false ;
+    printf("\n is walking:%d",iswalking);
+}
