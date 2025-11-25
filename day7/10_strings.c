0c9d9b9249bbe6ca6bd4a31198102ca6c9561540
bp2193191-create <bp2193191@gmail.com>
2025-11-25 15:44:03 +0530

    day7 code push

---
+++ day7/10_strings.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    char name[30] = "hello world";
+    printf("string is:%s", name);
+}
---
+++ day7/5_evenornot.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int arr[5], i;
+    printf("enter a numbers:\n");
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("array elements:\n");
+    for (int i = 0; i < 5; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            printf("%d value is :%d\n", i + 1, arr[i]);
+        }
+    }
+}
